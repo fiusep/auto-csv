d714f1b7c1d6e77c4dda22a5c2e290404f1e3ed6
fiusep <giuseppetaronnobi@gmail.com>
2023-01-22 23:17:17 +0100

    correzione controllo macchine

--- auto.cpp
+++ auto.cpp
@@ -7,46 +7,53 @@ struct automobile{
     string categoria, marca, modello, colore, giorni[7];
 };
 
-void controllo(automobile affitto[], string cat, string gio[], int c)
+void controllo(automobile affitto[], string cat, string gio[], int c, int magg)
 {
-    int x = 0;
+    int x = 0, scl;
 
     ifstream fin(file);
 
     while(!fin.eof())
     {
         getline (fin, affitto[x].categoria, ',');
-        cout<<affitto[x].categoria;
         getline (fin, affitto[x].marca, ',');
-        cout<<affitto[x].marca;
         getline (fin, affitto[x].modello, ',');
-        cout<<affitto[x].modello;
         getline (fin, affitto[x].colore, ',');
-        cout<<affitto[x].colore;
 
         for(int j = 0; j < 7; j++){
                 if(j == 6)
                     getline (fin, affitto[x].giorni[j]);
                 else getline (fin, affitto[x].giorni[j], ',');
-            cout<<affitto[x].giorni[j];
         }
         x++;
         cout<<endl;
     }
 
+    system("cls");
+
     cout<<"le macchine che potrebbero interessarti sono: "<<endl;
+
+    bool check = false;
 
     for(int a = 0; a <= c; a++)
     {
         if (cat == affitto[a].categoria){
             for(int g = 0; g < 7; g++){
-                if(gio[g] == to_string(g) && affitto[a].giorni[g] == "L"){
-                    affitto[a].giorni[g] = "A";
-                    }
+                if(gio[g] != "" && affitto[a].giorni[g] == " L"){
+                    affitto[a].giorni[g] = " A";
+                    check = true;
+                }
+                if (check == true && g == magg){
+                    cout<<a+1<<">"<<affitto[a].marca<<affitto[a].modello<<affitto[a].colore<<endl;
+                    check = false;
+                }
             }
-            cout<<a+1<<"> "<<affitto[a].marca<<" "<<affitto[a].modello<<" "<<affitto[a].colore<<endl;
         }
     }
+
+    cout<<"0> Nessuna di queste\n>> ";
+    cin>>scl;
+
     cout<<endl;
 }
 
@@ -76,7 +83,7 @@ int menu()
         cin>>cat;
         }while(cat != "utilitaria" && cat != "media" && cat != "lusso" && cat != "sportiva" && cat != "furgone");
 
-        int cont;
+        int cont, magg = 0;
 
         do
         {
@@ -88,14 +95,17 @@ int menu()
                 if(cont >= 8 || cont < 0)
                     cout<<"scelta non valida"<<endl;
 
-                if (cont > 0 && cont < 8)
+                if (cont > 0 && cont < 8){
                     gio[cont-1] = to_string(cont);
+                    if (cont > magg)
+                        magg = cont;
+                }
 
             }while(cont >= 8 || cont < 0);
         }while(cont != 0);
         cout<<endl;
 
-        controllo(affitto, cat, gio, c);
+        controllo(affitto, cat, gio, c, magg);
     }
 }
 
